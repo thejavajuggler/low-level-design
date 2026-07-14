3d139ad188ed223fbacd21d31df1a41154f27fab
rebonishamma <rebonishamma@gmail.com>
2026-07-14 13:48:25 +0530

    Initial commit - Low Level Design project

--- src/main/java/ticTacToe/TicTacToe.java
+++ src/main/java/ticTacToe/TicTacToe.java
@@ -4,6 +4,7 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         Player player1 = new Player("Player1", Piece.X);
+
         Player player2 = new Player("Player2", Piece.O);
     }
 }
