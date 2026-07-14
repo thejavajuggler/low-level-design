eb2f65888ec6ce23414264ecf022cc9e06b805b4
rebonishamma <rebonishamma@gmail.com>
2026-07-14 13:39:16 +0530

    Initial commit - Low Level Design project

---
+++ src/main/java/ticTacToe/Player.java
@@ -0,0 +1,12 @@
+package ticTacToe;
+
+public class Player {
+
+    String name;
+    Piece piece;
+
+    public Player(String name, Piece piece) {
+        this.name = name;
+        this.piece = piece;
+    }
+}
---
+++ src/main/java/ticTacToe/TicTacToe.java
@@ -0,0 +1,9 @@
+package ticTacToe;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        Player player1 = new Player("Player1", Piece.X);
+        Player player2 = new Player("Player2", Piece.O);
+    }
+}
